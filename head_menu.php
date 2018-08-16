7aa1ad049475ed364e376c5a36972c6d50501db7
Petrlev <levendeevp@gmail.com>
2018-08-16 14:25:53 +0300

    Шапка - новая логика

--- head_menu.php
+++ head_menu.php
@@ -10,16 +10,17 @@
   }
   ?>
     <td align="left" valign="top">
-    <div>
-<div style="position: relative"><a href="#" id="modal1ChangeUser" style="font-size: 18px;">Мой кошелек</a> <a>&#9660;</a></div>
-<div style="margin: 10px 0 0 0; text-align:right"><a href="#" class="editUserTitle" style="font-size: 11px;">Изменить название</a></div>
-    </div>
+<div style="position: relative; margin: 20px 0 0 0; cursor:pointer" id="modal1ChangeUser">
+<a style="display:block; font-size: 15px; margin: 5px 0 5px 0; padding: 0 0 5px 0; border-bottom: 1px solid #DDD"><strong>Иванов Иван</strong> &#9660;</a>
+<a style="display:block; font-size: 11px;"><strong>Старший кошелек</strong></a>
+</div>
       
       <div class="modal1 modal1ChangeUser" style="margin:-10px 0 0 0">
 <div style="text-align:right"><a href="#" class="modal1Close"><img src="css/img/close.svg"></a></div>
 
 <a href="#" class="content_menu_generalUser" style="display:block; padding: 15px 0; border-bottom: 1px solid #e6e6e6">Мой кошелек</a>
-<a href="#" class="content_menu_juniorUser" style="display:block; padding: 15px 0">Мой семейный кошелек<br>(Иванов Иван)</a>
+<a href="#" class="content_menu_juniorUser" style="display:block; padding: 15px 0">Мой семейный кошелек
+<div style="display:block; font-size: 11px; margin: 5px 0 0 0">Старший пользователь: <strong>Иванов Константин</strong></div></a>
 
 </div>
 
@@ -37,7 +38,7 @@ $(".content_menu_generalUser").click(function()
 	$("div#content_menu_juniorUser").hide();
 	$("div#content_menu_generalUser").show();
 	$(".modal1").hide();
-	document.getElementById('modal1ChangeUser').innerHTML = 'Мой кошелек';
+	document.getElementById('modal1ChangeUser').innerHTML = '<a style="display:block; font-size: 15px; margin: 5px 0 5px 0; padding: 0 0 5px 0; border-bottom: 1px solid #DDD"><strong>Иванов Иван</strong> &#9660;</a><a style="display:block; font-size: 11px;"><strong>Старший кошелек</strong></a>';
 });
 
 $(".content_menu_juniorUser").click(function()
@@ -45,7 +46,7 @@ $(".content_menu_juniorUser").click(function()
 	$("div#content_menu_juniorUser").show();
 	$("div#content_menu_generalUser").hide();
 	$(".modal1").hide();
-	document.getElementById('modal1ChangeUser').innerHTML = 'Мой семейный кошелек<br><span style="font-size: 11px;">Старший пользователь: <strong>Иванов Константин</strong></span>';
+	document.getElementById('modal1ChangeUser').innerHTML = '<div style="position: relative; margin: 20px 0 0 0; cursor:pointer" id="modal1ChangeUser"><a style="display:block; font-size: 15px; margin: 5px 0 0 0; padding: 0 0 5px 0; border-bottom: 1px solid #DDD""><strong>Мой семейный кошелек</strong> &#9660;</a><a style="display:block; font-size: 11px; margin: 5px 0 0 0"><strong>Младший кошелек</strong><a style="display:block; font-size: 11px; margin: 5px 0 0 0">Старший пользователь: <strong>Иванов Константин</strong></a></div>';
 });
 
 $(".editUserTitle").click(function() {$(".modal1ChangeTitle").show();});
